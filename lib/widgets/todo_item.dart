d40772cfc9b61c03a2df3a5828f1b7b80e1cce79
Asmit Gupta <asmitwrites@gmail.com>
2022-10-17 14:51:18 +0530

    Added UI of search box, menu and profile picture along with app bar

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+      ),
+    );
+  }
+}
